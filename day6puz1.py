9bbfc621b46fd9311b1c6f272841bd517d3ddd01
Rodrigo Carrillo-Sanchez <rodrigo_carrillo_sanchez@homedepot.com>
2019-12-06 01:26:21 -0500

    figured out day 6 part 1

--- day6puz1.py
+++ day6puz1.py
@@ -33,12 +33,12 @@ def getPath(item, obj):
 def prettyPrint(obj, numTabs):
     # for visualization purposes
     if len(obj.keys()) == 0:
-        print('  ' * numTabs + '{}')
+        print('..' * numTabs + '{}')
     else:
         for key in obj.keys():
-            print('  ' * numTabs + '%s : {' %key)
+            print('..' * numTabs + '%s : {' %key)
             prettyPrint(obj[key], numTabs + 1)
-            print('  ' * numTabs + '}')
+            print('..' * numTabs + '}')
 
 knownMasses = set()
 orbitsDict = dict()
@@ -48,17 +48,28 @@ def chartOrbits():
         knownMasses.add(orbit[1])
         path = getPath(orbit[0], orbitsDict)
         path2 = getPath(orbit[1], orbitsDict)
-        if path2 is not None:
-            print('weird shit is going on')
-        if path is None:
+        if path is None and path2 is None:
             orbitsDict[orbit[0]] = dict()
             orbitsDict[orbit[0]][orbit[1]] = dict()
-        else:
+        elif path is None and path2 is not None:
+            orbitsDict[orbit[0]] = dict()
+            orbitsDict[orbit[0]][orbit[1]] = orbitsDict[orbit[1]]
+            del(orbitsDict[orbit[1]])
+        elif path is not None and path2 is None:
             pathEnd = orbitsDict
             for step in path:
                 pathEnd = pathEnd[step]
             pathEnd[orbit[1]] = dict()
+        elif path is not None and path2 is not None:
+            pathEnd = orbitsDict
+            for step in path:
+                pathEnd = pathEnd[step]
+            pathEnd[orbit[1]] = dict()
+            pathEnd[orbit[1]] = orbitsDict[orbit[1]]
+            del(orbitsDict[orbit[1]])
+
     
 
 chartOrbits()
-print(sum([len(getPath(mass, orbitsDict)) for mass in knownMasses]))
+prettyPrint(orbitsDict['COM'], 0)
+print(sum([len(getPath(mass, orbitsDict)) - 1 for mass in knownMasses]))
